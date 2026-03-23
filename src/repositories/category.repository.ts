4d5f91129ceedc90b448601742276dcc6abf3857
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-03-23 13:33:57 +0700

    feat: implement CRUD operations for categories in the repository

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -1,9 +1,25 @@
+import { eq } from "drizzle-orm";
 import db from "../db/db";
 import { categories } from "../db/schema";
 
 const CategoryRepository = {
   get: async () => {
-    return await db.select().from(categories);
+    return db.select().from(categories);
+  },
+
+  create: async (name: string) => {
+    await db.insert(categories).values({ name });
+  },
+
+  update: async (categoryId: number, name: string) => {
+    return db
+      .update(categories)
+      .set({ id: categoryId, name })
+      .where(eq(categories.id, categoryId));
+  },
+
+  delete: async (categoryId: number) => {
+    return db.delete(categories).where(eq(categories.id, categoryId));
   },
 };
 
